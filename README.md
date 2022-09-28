95151cd534a47ed56e21d9c2047ff5bf9b1b083a
RontiOluwa <olaoluwaojedapo@gmail.com>
2022-09-28 13:25:53 +0100

    feat: first commit for shadlabs subgraph

---
+++ README.md
@@ -0,0 +1,3 @@
+# SubGraph for Georli 0x5ECdbfD7b8E2c8D669967ED4eD1B390474c17f6F
+
+SubGrapgh_URL : (https://api.studio.thegraph.com/query/35550/shadlad-cars/0.1).
--- src/cars.ts
+++ src/cars.ts
@@ -1,26 +1,26 @@
 import {
   CarsItem as CarsItemEvent,
-  InspectionItem as InspectionItemEvent
-} from "../generated/Cars/Cars"
-import { CarsItem, InspectionItem } from "../generated/schema"
+  InspectionItem as InspectionItemEvent,
+} from "../generated/Cars/Cars";
+import { CarsItem, InspectionItem } from "../generated/schema";
 
 export function handleCarsItem(event: CarsItemEvent): void {
   let entity = new CarsItem(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.itemId = event.params.itemId
-  entity.VIN = event.params.VIN
-  entity.name = event.params.name
-  entity.color = event.params.color
-  entity.save()
+  );
+  entity.itemId = event.params.itemId;
+  entity.VIN = event.params.VIN;
+  entity.name = event.params.name;
+  entity.color = event.params.color;
+  entity.save();
 }
 
 export function handleInspectionItem(event: InspectionItemEvent): void {
   let entity = new InspectionItem(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._itemId = event.params._itemId
-  entity.VIN = event.params.VIN
-  entity.date = event.params.date
-  entity.save()
+  );
+  entity._itemId = event.params._itemId;
+  entity.VIN = event.params.VIN;
+  entity.date = event.params.date;
+  entity.save();
 }
